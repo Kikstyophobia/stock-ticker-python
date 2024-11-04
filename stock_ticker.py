02c523baf64a89d5db6c26fa1bf7c5d002be788d
Calvin Chung <calvin-chung@live.com>
2024-11-03 16:07:24 -0800

    TwelveData Api endpoints are working. Currently getting the desired data

---
+++ stock_ticker.py
@@ -0,0 +1,31 @@
+import requests
+import time
+import constants
+
+ticker = 'MSFT'
+api_key = constants.API_KEY
+
+def get_stock_data(ticker_symbol, api):
+    # url = f"https://api.twelvedata.com/price?symbol={ticker_symbol}&apikey={api}"
+    url = f"https://api.twelvedata.com/quote?symbol={ticker_symbol}&apikey={api}"
+    response = requests.get(url).json()
+    name = response['name']
+    exchange = response['exchange']
+    currency = response['currency']
+    datetime = response['datetime']
+    high = response['high'][:-3]
+    low = response['low'][:-3]
+    open_price = response['open'][:-3]
+    close_price = response['close'][:-3]
+    percent_change = response['percent_change'][:-3]
+    fifty_two_week_low = response['fifty_two_week']['low'][:-3]
+    fifty_two_week_high = response['fifty_two_week']['high'][:-3]
+    fifty_two_week_low_change = response['fifty_two_week']['low_change'][:-3]
+    fifty_two_week_high_change = response['fifty_two_week']['high_change'][:-3]
+    fifty_two_week_low_change_percent = response['fifty_two_week']['low_change_percent'][:-3]
+    fifty_two_week_high_change_precent = response['fifty_two_week']['high_change_percent'][:-3]
+
+    print(name, open_price, close_price)
+
+get_stock_data(ticker, api_key)
+    
